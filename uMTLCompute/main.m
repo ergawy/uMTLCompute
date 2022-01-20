7bfba2d050c169e6aaa5d00ab0f7b5f386055edb
ergawy <kareem.ergawy@guardsquare.com>
2022-01-20 15:30:14 +0100

    Initial Commit

---
+++ uMTLCompute/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  uMTLCompute
+//
+//  Created by Kareem Ergawy on 20.01.22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
